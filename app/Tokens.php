8f26a0f3ba27950cdb98bc6969a3e4dd2b37dc00
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-01-12 10:31:03 -0600

    tokens: allow completely overriding the url
    * Add a file named the same as the zip with .url on the end which
      will contain (on the first line) the entire path to the zip
    
    * The practical application here would be if you want users to pull
      builds from a different server in certain situations

--- app/Tokens.php
+++ app/Tokens.php
@@ -58,7 +58,11 @@
                     $api_level = intval($this->getBuildPropValue($buildpropArray, 'ro.build.version.sdk'));
                     $incremental = $this->getBuildPropValue($buildpropArray, 'ro.build.version.incremental');
                     $timestamp = intval($this->getBuildPropValue($buildpropArray, 'ro.build.date.utc'));
-                    $url = 'https://' . $_SERVER['HTTP_HOST'] . '/_builds/' . explode("/", $filePath)[4];
+                    if (file_exists($filePath.'.url')) {
+                        $url = explode("\n", file_get_contents($filePath.'.url'))[0];
+                    } else {
+                        $url = 'https://' . $_SERVER['HTTP_HOST'] . '/_builds/' . explode("/", $filePath)[4];
+                    }
                     $cache = array($device, $api_level, $incremental, $timestamp, Utils::getMD5($filePath), $url);
                     $mc->set($filePath, $cache);
                     $mc->set($incremental, array($device, $channel, $filePath));
